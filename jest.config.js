db22d293b181156f707b89079c4641a2e87c8cb7
Wasiu Idowu <hoxtygen@live.com>
2022-10-17 19:33:46 +0100

    Remove commented out line

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,14 @@
-// jest.config.js
-const nextJest = require('next/jest')
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
-  dir: './',
-})
+  dir: "./",
+});
 
 /** @type {import('jest').Config} */
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleDirectories: ['node_modules', '/'],
-  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleDirectories: ["node_modules", "/"],
+  testEnvironment: "jest-environment-jsdom",
   coverageThreshold: {
     global: {
       branches: 100,
@@ -19,7 +18,7 @@ const customJestConfig = {
     },
   },
   preset: "ts-jest",
-  testEnvironment:"jsdom"
-}
+  testEnvironment: "jsdom",
+};
 
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
